3ccd0d4521b4a979d7020796ec67058861d4b199
josura <giorgiolocicero@hotmail.com>
2024-10-22 09:07:01 +0200

    removed circular dependency on utilities in matrix, implementing approcimately equal directly in the Matrix class

--- src/data_structures/Matrix.hxx
+++ src/data_structures/Matrix.hxx
@@ -4,8 +4,8 @@
 #include <iterator>
 #include <stdexcept>
 #include <vector>
-#include "utils/utilities.hxx"
 #include <armadillo>
+#include <cmath>
 
 template <typename T>
 class Matrix {
@@ -140,6 +140,12 @@ class Matrix {
         arma::Col<T> asArmadilloColumnVector()const;
         arma::Row<T> asArmadilloRowVector()const;
         void printMatrix()const;
+
+
+        bool approximatelyEqual(float a, float b, float epsilon)
+        {
+            return fabs(a - b) <= ( (fabs(a) < fabs(b) ? fabs(b) : fabs(a)) * epsilon);
+        }
 
     protected:
         int rows_, cols_;
