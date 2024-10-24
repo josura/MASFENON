deae1ede75bcac91771b05a4a319eacd9808bb3e
josura <giorgiolocicero@hotmail.com>
2024-10-24 20:37:55 +0200

    init some functions to start the porting

---
+++ src/utils/mathUtilities.hxx
@@ -0,0 +1,17 @@
+#pragma once
+
+/**
+ * \brief   Generate a random integer number between min and max
+ * \return  the random number
+*/
+int randomNumber(int min, int max);
+/**
+ * \brief   Generate a random real number between min and max
+ * \return  the random number
+*/
+double randomRealNumber(double min, double max);
+/**
+ * \brief   Generate a random character between A and Z
+ * \return  the random character
+*/
+char generateRandomCharacter();
