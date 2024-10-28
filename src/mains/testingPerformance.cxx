040b798cbe1ff4068f45d4d1041202af1bbdc0ec
josura <giorgiolocicero@hotmail.com>
2024-10-28 14:23:50 +0100

    string utilities file init

--- src/mains/testingPerformance.cxx
+++ src/mains/testingPerformance.cxx
@@ -8,7 +8,6 @@
 using namespace std;
 
 int main() {
-
     auto matrix1 = Matrix<double>::createRandom(4,8);
     auto matrix2 = Matrix<double>::createRandom(8,5);
     auto matrixres=Matrix<double>(4,5);
