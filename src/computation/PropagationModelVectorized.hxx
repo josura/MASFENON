98df8ce95b47bcb0ce38aee107a1da9efdaa8183
josura <giorgiolocicero@hotmail.com>
2024-11-20 14:14:06 +0100

    written header file for propagation model abstract class

---
+++ src/computation/PropagationModelVectorized.hxx
@@ -0,0 +1,19 @@
+#pragma once
+#include <armadillo>
+#include <functional>
+#include <vector>
+#include "data_structures/WeightedEdgeGraph.hxx"
+
+class PropagationModelVectorized{
+    protected:
+        std::function<double(double)> scaleFunction;
+    public:
+        virtual ~PropagationModelVectorized(){}
+        //using the scale function as a parameter itself, dependency injection
+        virtual arma::Col<double> propagate(arma::Col<double> input, double time) = 0;
+        virtual arma::Col<double> propagationTerm(arma::Col<double> input, double time) = 0;
+
+        //getters and setters
+        std::function<double(double)> getScaleFunction(){return this->scaleFunction;}
+        void setScaleFunction(std::function<double(double)> scaleFunction){this->scaleFunction = scaleFunction;}
+};
