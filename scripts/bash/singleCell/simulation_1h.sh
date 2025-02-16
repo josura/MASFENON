471b02a0f68eacff3d1aaeb57763b069466f6378
josura <giorgiolocicero@hotmail.com>
2025-02-16 16:59:04 +0000

    continuing to write the script to start the simulation for single cell data, TODO finish the creation of the graphs in the nodes folder

--- scripts/bash/singleCell/simulation_1h.sh
+++ scripts/bash/singleCell/simulation_1h.sh
@@ -1,6 +1,7 @@
 graphsFolder="/home/josura/Projects/ccc/datiIdo/inputGraphs/1h/graphs"
 initialPerturbationFolder="/home/josura/Projects/ccc/datiIdo/inputGraphs/1h/nodeValues"
 typeInteractionsFolder="/home/josura/Projects/ccc/datiIdo/inputGraphs/1h/interactions"
+nodesFolder="/home/josura/Projects/ccc/datiIdo/inputGraphs/1h/nodes"
 
 mpirun --mca pml ob1 --mca btl tcp,self --mca btl_tcp_if_include wlan0 -np 12 ./build/c2c-sepia-MPI --graphsFilesFolder $graphsFolder \
         --initialPerturbationPerTypeFolder $initialPerturbationFolder \
