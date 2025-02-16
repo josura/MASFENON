f867b7ec254b4d7c146192e83e6581b95e33be5c
josura <giorgiolocicero@hotmail.com>
2025-02-16 16:57:24 +0000

    continuing to write the script to start the simulation for single cell data

---
+++ scripts/bash/singleCell/simulation_1h.sh
@@ -0,0 +1,20 @@
+graphsFolder="/home/josura/Projects/ccc/datiIdo/inputGraphs/1h/graphs"
+initialPerturbationFolder="/home/josura/Projects/ccc/datiIdo/inputGraphs/1h/nodeValues"
+typeInteractionsFolder="/home/josura/Projects/ccc/datiIdo/inputGraphs/1h/interactions"
+
+mpirun --mca pml ob1 --mca btl tcp,self --mca btl_tcp_if_include wlan0 -np 12 ./build/c2c-sepia-MPI --graphsFilesFolder $graphsFolder \
+        --initialPerturbationPerTypeFolder $initialPerturbationFolder \
+        --typeInteractionFolder $typeInteractionsFolder \
+        --nodeDescriptionFolder $nodesFolder \
+        --dissipationModel scaled \
+        --dissipationModelParameters $dissipationScaleFactor \
+        --propagationModel neighbors \
+        --propagationModelParameters $propagationScaleFactor \
+        --intertypeIterations 20 \
+        --intratypeIterations 5 \
+        --timestep 0.1 \
+        --virtualNodesGranularity typeAndNode \
+        --saturation \
+        --undirectedEdges \
+        --undirectedTypeEdges \
+        --outputFolder $outputFolder"
