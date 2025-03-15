9c483c8eb11f94549f1044cb8c9c800be14872d0
josura <giorgiolocicero@hotmail.com>
2025-03-15 20:01:09 +0000

    init simulation for biological data with ligand receptors

---
+++ scripts/bash/singleCell/simulation_1h_withLR.sh
@@ -0,0 +1,22 @@
+#!  /bin/bash
+
+outputFolder="/home/josura/Projects/ccc/datiIdo/inputGraphs/1h/outputWithLR"
+dissipationScaleFactor=0.2
+propagationScaleFactor=0.5
+
+mpirun --mca pml ob1 --mca btl tcp,self --mca btl_tcp_if_include wlan0 -np 12 ./build/masfenon-MPI --graphsFilesFolder $graphsFolder \
+        --initialPerturbationPerTypeFolder $initialPerturbationFolder \
+        --typeInteractionFolder $typeInteractionsFolder \
+        --nodeDescriptionFolder $nodesFolder \
+        --dissipationModel scaled \
+        --dissipationModelParameters $dissipationScaleFactor \
+        --propagationModel neighbors \
+        --propagationModelParameters $propagationScaleFactor \
+        --intertypeIterations 20 \
+        --intratypeIterations 5 \
+        --timestep 0.1 \
+        --virtualNodesGranularity typeAndNode \
+        --saturation \
+        --undirectedEdges \
+        --undirectedTypeEdges \
+        --outputFolder $outputFolder
