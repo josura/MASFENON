ba1da507da99fa83efdda5b5fe502a2b720077e4
josura <giorgiolocicero@hotmail.com>
2025-04-01 15:45:36 +0000

    add: script for simulation in general

---
+++ scripts/bash/singleCell/simulation_general_withLR.sh
@@ -0,0 +1,28 @@
+#!  /bin/bash
+graphsFolder="/home/josura/Projects/ccc/datiIdo/inputGraphs/1h/graphsWithLR"
+initialPerturbationFolder="/home/josura/Projects/ccc/datiIdo/inputGraphs/1h/nodeValuesWithLR"
+typeInteractionsFolder="/home/josura/Projects/ccc/datiIdo/inputGraphs/1h/interactionsWithLR"
+nodesFolder="/home/josura/Projects/ccc/datiIdo/inputGraphs/1h/nodesWithLR"
+
+outputFolder="/home/josura/Projects/ccc/datiIdo/inputGraphs/1h/outputWithLR"
+dissipationScaleFactor=0.2
+propagationScaleFactor=0.5
+
+
+mpirun --mca pml ob1 --mca btl tcp,self --mca btl_tcp_if_include wlan0 -np 8 ./build/masfenon-MPI \
+        --graphsFilesFolder $graphsFolder \
+        --initialPerturbationPerTypeFolder $initialPerturbationFolder \
+        --typeInteractionFolder $typeInteractionsFolder \
+        --nodeDescriptionFolder $nodesFolder \
+        --dissipationModel scaled \
+        --dissipationModelParameters $dissipationScaleFactor \
+        --propagationModel neighbors \
+        --propagationModelParameters $propagationScaleFactor \
+        --intertypeIterations 10 \
+        --intratypeIterations 5 \
+        --timestep 1 \
+        --virtualNodesGranularity typeAndNode \
+        --saturation \
+        --saturationTerm 0.05 \
+        --outputFormat iterationMatrix \
+        --outputFolder $outputFolder
