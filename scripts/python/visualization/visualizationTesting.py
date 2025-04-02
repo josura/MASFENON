fafbf8483e5ec6ef5527c9c4c502ba81c67c6ef6
josura <giorgiolocicero@hotmail.com>
2025-04-02 10:58:43 +0000

    feat: added example network name

--- scripts/python/visualization/visualizationTesting.py
+++ scripts/python/visualization/visualizationTesting.py
@@ -21,33 +21,34 @@ nodesFile = "/home/josura/Projects/ccc/datiIdo/inputGraphs/1h/nodeValuesWithLR/A
 edgesFile = "/home/josura/Projects/ccc/datiIdo/inputGraphs/1h/graphsWithLR/AT1-metabolites.tsv"
 nodes_df = pd.read_csv(nodesFile, sep="\t")
 edges_df = pd.read_csv(edgesFile, sep="\t")
+network_name = "AT1-metabolites"
 g_complete =net.Network(height='600px',width='50%',
               bgcolor='white',font_color="red",notebook=True,
-              heading="An example Graph for" + ,directed=True)
+              heading="An example Graph for" + network_name,directed=True)
  
-colors=[]
-for i in range(1,11):  
-  c = generate_color()
-  while(c in colors):
-      c = generate_color()
-  colors.append(c)
+# colors=[]
+# for i in range(1,11):  
+#   c = generate_color()
+#   while(c in colors):
+#       c = generate_color()
+#   colors.append(c)
  
-  val = generate_size_node()
-  b = random.randint(3,5)
+#   val = generate_size_node()
+#   b = random.randint(3,5)
  
-  g_complete.add_node(i,label=str(i),color=c,value=val,
-                      title="Hello! I am Node "+str(i),borderWidth=b)
+#   g_complete.add_node(i,label=str(i),color=c,value=val,
+#                       title="Hello! I am Node "+str(i),borderWidth=b)
  
-i=0
-chosen_set = []
-while(i!=20):
-  eg = generate_edge()
-  if(eg[0]!=eg[1] and not (eg in chosen_set)):
-      chosen_set.append(eg)
-      g_complete.add_edge(eg[0],eg[1])
-      i+=1
+# i=0
+# chosen_set = []
+# while(i!=20):
+#   eg = generate_edge()
+#   if(eg[0]!=eg[1] and not (eg in chosen_set)):
+#       chosen_set.append(eg)
+#       g_complete.add_edge(eg[0],eg[1])
+#       i+=1
  
-g_complete.show_buttons(['physics'])
+# g_complete.show_buttons(['physics'])
  
 g_complete.show('A_Complete_Networkx_Graph.html')
 display(HTML('A_Complete_Networkx_Graph.html'))
