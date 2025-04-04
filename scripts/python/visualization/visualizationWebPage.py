5bb6ae827d3c75a3873e65e1014b0271d5670ea5
josura <giorgiolocicero@hotmail.com>
2025-04-04 10:56:34 +0000

    feat: reading the files to plot for the graphs

--- scripts/python/visualization/visualizationWebPage.py
+++ scripts/python/visualization/visualizationWebPage.py
@@ -7,8 +7,40 @@ import pandas as pd
 
 app = Flask(__name__)
 
-# Example time series data
-timepoints = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# load the graph nodes and edges from
+nodesFile = "/home/josura/Projects/ccc/datiIdo/inputGraphs/1h/nodesWithLR/AT1-metabolites.tsv"
+edgesFile = "/home/josura/Projects/ccc/datiIdo/inputGraphs/1h/graphsWithLR/AT1-metabolites.tsv"
+nodes_df = pd.read_csv(nodesFile, sep="\t")
+edges_df = pd.read_csv(edgesFile, sep="\t")
+network_name = "AT1-metabolites"
+g_from_nx = net.Network(height='600px',width='50%',
+              bgcolor='white',font_color="red",notebook=True,
+              heading="An example Graph for " + network_name,directed=True)
+
+# load the time series data for the values of the nodes through time
+timeSeriesFile = "/home/josura/Projects/ccc/datiIdo/inputGraphs/1h/multipleOutputsWithLR/dissipation_0.3-propagation_0.3-conservation_0.3/iterationMatrices/AT1-metabolites.tsv"
+timeSeries_df = pd.read_csv(timeSeriesFile, sep="\t")
+## preprocess the time series data
+### drop the last column (useless)
+timeSeries_df = timeSeries_df.drop(timeSeries_df.columns[-1], axis=1)
+## first row of the matrix is the node names (nodeNames), the rest of the columns are the timepoints (the names go from 0 to the end of the simulation timepoint)
+nodeNames = timeSeries_df['nodeNames']
+## drop the nodeNames column
+timeSeries_df = timeSeries_df.drop('nodeNames', axis=1)
+## get the timepoints
+timepoints = timeSeries_df.columns
+## we need to transpose the matrix so that the columns are the node names and the rows are the timepoints iteration results
+timeSeries_df = timeSeries_df.transpose()
+## set the column names to be the node names
+timeSeries_df.columns = nodeNames
+## set the index to be the timepoints
+timeSeries_df.index = timepoints
+## add a column to be the timepoints
+# temp_iterationMatrix['time'] = timepoints
+## change index name to be 'time'
+timeSeries_df.index.name = 'time'
+
+allNodes = timeSeries_df.columns.tolist()
 
 # Function generating Plotly plot for each timepoint
 def create_plot(timepoint):
