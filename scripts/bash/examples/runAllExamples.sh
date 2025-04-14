7a6f8b2651b0b4bb239f973bd74f37b54ec18cad
josura <giorgiolocicero@hotmail.com>
2025-04-14 12:28:23 +0000

    feat: added interface to run all example bash script

--- scripts/bash/examples/runAllExamples.sh
+++ scripts/bash/examples/runAllExamples.sh
@@ -6,7 +6,7 @@ fi
 
 # run the examples
 ## homogeneous graphs
-mpirun --mca pml ob1 --mca btl tcp,self --mca btl_tcp_if_include -np 2 ./build/masfenon-MPI --fUniqueGraph data/testdata/testHomogeneousGraph/edges-Graph1-general.tsv \
+mpirun --mca pml ob1 --mca btl tcp,self --mca btl_tcp_if_include wlan0 -np 2 ./build/masfenon-MPI --fUniqueGraph data/testdata/testHomogeneousGraph/edges-Graph1-general.tsv \
             --fInitialPerturbation data/testdata/testHomogeneousGraph/initialValues-general.tsv \
             --typeInteractionFolder data/testdata/testHomogeneousGraph/interactions \
             --dissipationModel scaled \
@@ -19,7 +19,7 @@ mpirun --mca pml ob1 --mca btl tcp,self --mca btl_tcp_if_include -np 2 ./build/m
             --outputFolder /tmp/outputExamples/testingMPIsingle \
             --outputFormat iterationMatrix
 
-mpirun --mca pml ob1 --mca btl tcp,self --mca btl_tcp_if_include -np 2 ./build/masfenon-MPI --fUniqueGraph data/testdata/testHomogeneousGraph/edges-Graph1-general.tsv \
+mpirun --mca pml ob1 --mca btl tcp,self --mca btl_tcp_if_include wlan0 -np 2 ./build/masfenon-MPI --fUniqueGraph data/testdata/testHomogeneousGraph/edges-Graph1-general.tsv \
             --fInitialPerturbation data/testdata/testHomogeneousGraph/initialValues-general.tsv \
             --typeInteractionFolder data/testdata/testHomogeneousGraph/interactions \
             --dissipationModel scaled \
@@ -33,7 +33,7 @@ mpirun --mca pml ob1 --mca btl tcp,self --mca btl_tcp_if_include -np 2 ./build/m
             --outputFolder /tmp/outputExamples/testingMPISingleGranular \
             --outputFormat iterationMatrix
 
-mpirun --mca pml ob1 --mca btl tcp,self --mca btl_tcp_if_include -np 2 ./build/masfenon-MPI --fUniqueGraph data/testdata/testHomogeneousGraph/edges-Graph1-general.tsv \
+mpirun --mca pml ob1 --mca btl tcp,self --mca btl_tcp_if_include wlan0 -np 2 ./build/masfenon-MPI --fUniqueGraph data/testdata/testHomogeneousGraph/edges-Graph1-general.tsv \
             --initialPerturbationPerTypeFolder data/testdata/testHomogeneousGraph/initialValues \
             --typeInteractionFolder data/testdata/testHomogeneousGraph/interactions \
             --dissipationModel scaled \
@@ -48,7 +48,7 @@ mpirun --mca pml ob1 --mca btl tcp,self --mca btl_tcp_if_include -np 2 ./build/m
             --outputFormat iterationMatrix
 
 ## heterogeneous graphs
-mpirun --mca pml ob1 --mca btl tcp,self --mca btl_tcp_if_include -np 2 ./build/masfenon-MPI --graphsFilesFolder data/testdata/testHeterogeneousGraph/graphs \
+mpirun --mca pml ob1 --mca btl tcp,self --mca btl_tcp_if_include wlan0 -np 2 ./build/masfenon-MPI --graphsFilesFolder data/testdata/testHeterogeneousGraph/graphs \
             --initialPerturbationPerTypeFolder data/testdata/testHeterogeneousTemporalGraph/initialValuesPartialTypes \
             --typeInteractionFolder data/testdata/testHeterogeneousTemporalGraph/interactions \
             --dissipationModel scaled \
@@ -62,22 +62,7 @@ mpirun --mca pml ob1 --mca btl tcp,self --mca btl_tcp_if_include -np 2 ./build/m
             --outputFolder /tmp/outputExamples/testingMPIgranularDifferentProcessors \
             --outputFormat iterationMatrix
 
-mpirun --mca pml ob1 --mca btl tcp,self --mca btl_tcp_if_include -np 2 ./build/masfenon-MPI --graphsFilesFolder data/testdata/testHeterogeneousGraph/graphs \
-            --initialPerturbationPerTypeFolder data/testdata/testHeterogeneousTemporalGraph/initialValuesPartialTypes \
-            --typeInteractionFolder data/testdata/testHeterogeneousTemporalGraph/interactions \
-            --dissipationModel scaled \
-            --dissipationModelParameters 0.2 \
-            --conservationModel scaled \
-            --conservationModelParameters 0.2 \
-            --propagationModel customPropagation \
-            --propagationModelParameters 0.2 \
-            --virtualNodesGranularity typeAndNode \
-            --saturation \
-            --nodeDescriptionFolder data/testdata/testHeterogeneousGraph/nodesDescriptionDifferentStructure \
-            --outputFolder /tmp/outputExamples/testingMPIgranularDifferentProcessors \
-            --outputFormat iterationMatrix
-
-mpirun --mca pml ob1 --mca btl tcp,self --mca btl_tcp_if_include -np 2 ./build/masfenon-MPI --graphsFilesFolder data/testdata/testHeterogeneousTemporalGraph/graphs \
+mpirun --mca pml ob1 --mca btl tcp,self --mca btl_tcp_if_include wlan0 -np 2 ./build/masfenon-MPI --graphsFilesFolder data/testdata/testHeterogeneousGraph/graphs \
             --initialPerturbationPerTypeFolder data/testdata/testHeterogeneousTemporalGraph/initialValuesPartialTypes \
             --typeInteractionFolder data/testdata/testHeterogeneousTemporalGraph/interactions \
             --dissipationModel scaled \
@@ -92,7 +77,22 @@ mpirun --mca pml ob1 --mca btl tcp,self --mca btl_tcp_if_include -np 2 ./build/m
             --outputFolder /tmp/outputExamples/testingMPIgranularDifferentProcessors \
             --outputFormat iterationMatrix
 
-mpirun --mca pml ob1 --mca btl tcp,self --mca btl_tcp_if_include -np 2 ./build/masfenon-MPI --graphsFilesFolder data/testdata/testHeterogeneousTemporalGraph/graphs \
+mpirun --mca pml ob1 --mca btl tcp,self --mca btl_tcp_if_include wlan0 -np 2 ./build/masfenon-MPI --graphsFilesFolder data/testdata/testHeterogeneousTemporalGraph/graphs \
+            --initialPerturbationPerTypeFolder data/testdata/testHeterogeneousTemporalGraph/initialValuesPartialTypes \
+            --typeInteractionFolder data/testdata/testHeterogeneousTemporalGraph/interactions \
+            --dissipationModel scaled \
+            --dissipationModelParameters 0.2 \
+            --conservationModel scaled \
+            --conservationModelParameters 0.2 \
+            --propagationModel customPropagation \
+            --propagationModelParameters 0.2 \
+            --virtualNodesGranularity typeAndNode \
+            --saturation \
+            --nodeDescriptionFolder data/testdata/testHeterogeneousGraph/nodesDescriptionDifferentStructure \
+            --outputFolder /tmp/outputExamples/testingMPIgranularDifferentProcessors \
+            --outputFormat iterationMatrix
+
+mpirun --mca pml ob1 --mca btl tcp,self --mca btl_tcp_if_include wlan0 -np 2 ./build/masfenon-MPI --graphsFilesFolder data/testdata/testHeterogeneousTemporalGraph/graphs \
             --initialPerturbationPerTypeFolder data/testdata/testHeterogeneousTemporalGraph/initialValuesPartialTypes \
             --typeInteractionFolder data/testdata/testHeterogeneousTemporalGraph/interactions \
             --dissipationModel scaled \
