df0c151c0c53973c730053362eb1a68a1fff0692
josura <giorgiolocicero@hotmail.com>
2025-04-14 12:11:30 +0000

    feat: init bash script to run all the examples in examples.md

---
+++ scripts/bash/examples/runAllExamples.sh
@@ -0,0 +1,107 @@
+# control if the output folder exists
+if [ ! -d "/tmp/outputExamples" ]; then
+    mkdir /tmp/outputExamples
+fi
+
+
+# run the examples
+## homogeneous graphs
+mpirun --mca pml ob1 --mca btl tcp,self --mca btl_tcp_if_include -np 2 ./build/masfenon-MPI --fUniqueGraph data/testdata/testHomogeneousGraph/edges-Graph1-general.tsv \
+            --fInitialPerturbation data/testdata/testHomogeneousGraph/initialValues-general.tsv \
+            --typeInteractionFolder data/testdata/testHomogeneousGraph/interactions \
+            --dissipationModel scaled \
+            --dissipationModelParameters 0.2 \
+            --conservationModel scaled \
+            --conservationModelParameters 0.2 \
+            --propagationModel customPropagation \
+            --propagationModelParameters 0.2 \
+            --saturation \
+            --outputFolder /tmp/outputExamples/testingMPIsingle \
+            --outputFormat iterationMatrix
+
+mpirun --mca pml ob1 --mca btl tcp,self --mca btl_tcp_if_include -np 2 ./build/masfenon-MPI --fUniqueGraph data/testdata/testHomogeneousGraph/edges-Graph1-general.tsv \
+            --fInitialPerturbation data/testdata/testHomogeneousGraph/initialValues-general.tsv \
+            --typeInteractionFolder data/testdata/testHomogeneousGraph/interactions \
+            --dissipationModel scaled \
+            --dissipationModelParameters 0.2 \
+            --conservationModel scaled \
+            --conservationModelParameters 0.2 \
+            --propagationModel customPropagation \
+            --propagationModelParameters 0.2 \
+            --virtualNodesGranularity typeAndNode \
+            --saturation \
+            --outputFolder /tmp/outputExamples/testingMPISingleGranular \
+            --outputFormat iterationMatrix
+
+mpirun -np 2 ./build/masfenon-MPI --fUniqueGraph data/testdata/testHomogeneousGraph/edges-Graph1-general.tsv \
+            --initialPerturbationPerTypeFolder data/testdata/testHomogeneousGraph/initialValues \
+            --typeInteractionFolder data/testdata/testHomogeneousGraph/interactions \
+            --dissipationModel scaled \
+            --dissipationModelParameters 0.2 \
+            --conservationModel scaled \
+            --conservationModelParameters 0.2 \
+            --propagationModel customPropagation \
+            --propagationModelParameters 0.2 \
+            --virtualNodesGranularity typeAndNode \
+            --saturation \
+            --outputFolder /tmp/outputExamples/testingMPISingleGranular \
+            --outputFormat iterationMatrix
+
+## heterogeneous graphs
+mpirun -np 2 ./build/masfenon-MPI --graphsFilesFolder data/testdata/testHeterogeneousGraph/graphs \
+            --initialPerturbationPerTypeFolder data/testdata/testHeterogeneousTemporalGraph/initialValuesPartialTypes \
+            --typeInteractionFolder data/testdata/testHeterogeneousTemporalGraph/interactions \
+            --dissipationModel scaled \
+            --dissipationModelParameters 0.2 \
+            --conservationModel scaled \
+            --conservationModelParameters 0.2 \
+            --propagationModel customPropagation \
+            --propagationModelParameters 0.2 \
+            --virtualNodesGranularity typeAndNode \
+            --saturation \
+            --outputFolder outputs/testingMPIgranularDifferentProcessors \
+            --outputFormat iterationMatrix
+
+mpirun -np 2 ./build/masfenon-MPI --graphsFilesFolder data/testdata/testHeterogeneousGraph/graphs \
+            --initialPerturbationPerTypeFolder data/testdata/testHeterogeneousTemporalGraph/initialValuesPartialTypes \
+            --typeInteractionFolder data/testdata/testHeterogeneousTemporalGraph/interactions \
+            --dissipationModel scaled \
+            --dissipationModelParameters 0.2 \
+            --conservationModel scaled \
+            --conservationModelParameters 0.2 \
+            --propagationModel customPropagation \
+            --propagationModelParameters 0.2 \
+            --virtualNodesGranularity typeAndNode \
+            --saturation \
+            --nodeDescriptionFolder data/testdata/testHeterogeneousGraph/nodesDescriptionDifferentStructure \
+            --outputFolder outputs/testingMPIgranularDifferentProcessors \
+            --outputFormat iterationMatrix
+
+mpirun -np 2 ./build/masfenon-MPI --graphsFilesFolder data/testdata/testHeterogeneousTemporalGraph/graphs \
+            --initialPerturbationPerTypeFolder data/testdata/testHeterogeneousTemporalGraph/initialValuesPartialTypes \
+            --typeInteractionFolder data/testdata/testHeterogeneousTemporalGraph/interactions \
+            --dissipationModel scaled \
+            --dissipationModelParameters 0.2 \
+            --conservationModel scaled \
+            --conservationModelParameters 0.2 \
+            --propagationModel customPropagation \
+            --propagationModelParameters 0.2 \
+            --virtualNodesGranularity typeAndNode \
+            --saturation \
+            --nodeDescriptionFolder data/testdata/testHeterogeneousGraph/nodesDescriptionDifferentStructure \
+            --outputFolder outputs/testingMPIgranularDifferentProcessors \
+            --outputFormat iterationMatrix
+
+mpirun -np 2 ./build/masfenon-MPI --graphsFilesFolder data/testdata/testHeterogeneousTemporalGraph/graphs \
+            --initialPerturbationPerTypeFolder data/testdata/testHeterogeneousTemporalGraph/initialValuesPartialTypes \
+            --typeInteractionFolder data/testdata/testHeterogeneousTemporalGraph/interactions \
+            --dissipationModel scaled \
+            --dissipationModelParameters 0.2 \
+            --conservationModel scaled \
+            --conservationModelParameters 0.2 \
+            --propagationModel customPropagation \
+            --propagationModelParameters 0.2 \
+            --virtualNodesGranularity typeAndNode \
+            --nodeDescriptionFolder data/testdata/testHeterogeneousGraph/nodesDescriptionDifferentStructure \
+            --outputFolder outputs/testingMPIgranularDifferentProcessors \
+            --outputFormat iterationMatrix
