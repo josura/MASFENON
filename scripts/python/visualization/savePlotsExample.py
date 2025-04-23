f088d63caec421db2b80bf4805f1adcd58e19501
josura <giorgiolocicero@hotmail.com>
2025-04-23 16:31:40 +0000

    feat: code to save the example plots for future use, TODO complete the code

--- scripts/python/visualization/savePlotsExample.py
+++ scripts/python/visualization/savePlotsExample.py
@@ -6,3 +6,11 @@ import pandas as pd
 import pickle
 import copy
 
+outputFolderHomogenous_example1 = "/tmp/outputExamples/testingMPIsingle"
+outputFolderHomogenous_example2 = "/tmp/outputExamples/testingMPISingleGranular"
+outputFolderHomogenous_example3 = "/tmp/outputExamples/testingMPISingleGranular-2"
+outputFolderHeterogenous_example1 = "/tmp/outputExamples/testingMPIgranularDifferentProcessors"
+outputFolderHeterogenous_example2 = "/tmp/outputExamples/testingMPIgranularDifferentProcessors-2"
+outputFolderHeterogenous_example3 = "/tmp/outputExamples/testingMPIgranularDifferentProcessors-3"
+outputFolderHeterogenous_example4 = "/tmp/outputExamples/testingMPIgranularDifferentProcessors-4"
+
