003b73459544b05312ab1706dc46a78ee76037f4
josura <giorgiolocicero@hotmail.com>
2025-05-12 14:25:43 +0000

    documentation: doxygen header for the DissipationModelRandom header file

--- src/computation/DissipationModelRandom.hxx
+++ src/computation/DissipationModelRandom.hxx
@@ -1,3 +1,11 @@
+/**
+ * @file DissipationModelRandom.hxx
+ * @ingroup Core
+ * @brief Defines the DissipationModelRandom class used for managing random dissipation dynamics for the computation of the perturbation in MASFENON.
+ * @details The DissipationModelRandom class inherits from the DissipationModel class and implements random dissipation logic.
+ * @details The random dissipation model uses a range of values to compute the dissipation term.
+ * @details The dissipation term is computed as a random value between the given range, for every single element.
+ */
 #pragma once
 #include <armadillo>
 #include "computation/DissipationModel.hxx"
