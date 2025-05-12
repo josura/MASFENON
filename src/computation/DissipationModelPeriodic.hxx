665c263a557f9622fc349b7572ad6eb90176c84a
josura <giorgiolocicero@hotmail.com>
2025-05-12 14:08:17 +0000

    documentation: doxygen header for the DissipationModelPeriodic file

--- src/computation/DissipationModelPeriodic.hxx
+++ src/computation/DissipationModelPeriodic.hxx
@@ -1,7 +1,17 @@
+/**
+ * @file DissipationModelPeriodic.hxx
+ * @ingroup Core
+ * @brief Defines the DissipationModelPeriodic class used for managing periodic dissipation dynamics for the computation of the perturbation in MASFENON.
+ * @details The DissipationModelPeriodic class inherits from the DissipationModel class and implements periodic dissipation logic.
+ * @details The periodic dissipation model uses a set of phases, periods, and amplitudes to compute the dissipation term.
+ * @details The dissipation term is computed as a sum of sine functions with the given phases, periods, and amplitudes.
+ * @see DissipationModel.hxx
+ */
 #pragma once
 #include <armadillo>
 #include "computation/DissipationModel.hxx"
 
+/** */
 class DissipationModelPeriodic : public DissipationModel
 {
     private:
