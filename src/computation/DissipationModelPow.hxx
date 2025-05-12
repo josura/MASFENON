5cea6824659e4183d3ca35f8392213e35f10e985
josura <giorgiolocicero@hotmail.com>
2025-05-12 14:16:46 +0000

    documentation: doxygen header for the DissipationModelPow header file

--- src/computation/DissipationModelPow.hxx
+++ src/computation/DissipationModelPow.hxx
@@ -1,3 +1,10 @@
+/**
+ * @file DissipationModelPow.hxx
+ * @ingroup Core
+ * @brief Defines the DissipationModelPow class used for managing power dissipation dynamics for the computation of the perturbation in MASFENON.
+ * @details The DissipationModelPow class inherits from the DissipationModel class and implements power dissipation logic.
+ * @details The power dissipation model uses a power factor to compute the dissipation term.
+ */
 #pragma once
 #include <armadillo>
 #include "computation/DissipationModel.hxx"
