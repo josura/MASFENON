4019a9cae692c3a023741226845920726e03b8e4
josura <giorgiolocicero@hotmail.com>
2025-05-12 14:40:45 +0000

    documentation: doxygen header for the PropagationModel header file

--- src/computation/PropagationModel.hxx
+++ src/computation/PropagationModel.hxx
@@ -1,3 +1,13 @@
+/**
+ * @file PropagationModel.hxx
+ * @ingroup Core
+ * @brief Defines the PropagationModel class used for managing propagation dynamics for the computation of the perturbation in MASFENON.
+ * @details The PropagationModel class provides methods for applying propagation logic to the perturbation computation.
+ * @details The propagation class uses a scale function to determine the scaling of the propagation term. The scale function can be set and modified as needed.
+ * @details To set the scale function, @see CustomFunctions.hxx
+ * @warning This class is an abstract class and should not be used directly. It is intended to be inherited by other classes that implement specific propagation models.
+ * @todo Make it stateful. Related to issue #28 @link
+ */
 #pragma once
 #include <armadillo>
 #include <functional>
