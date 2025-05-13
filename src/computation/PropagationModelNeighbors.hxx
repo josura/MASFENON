8f32523f5ed8e536ab9899a79c899679ff1bc07b
josura <giorgiolocicero@hotmail.com>
2025-05-13 13:19:19 +0000

    documentation: doxygen header for PropagationModelCustom.hxx

--- src/computation/PropagationModelNeighbors.hxx
+++ src/computation/PropagationModelNeighbors.hxx
@@ -1,3 +1,12 @@
+/**
+ * @file PropagationModelNeighbors.hxx
+ * @ingroup Core
+ * @brief Defines the PropagationModelNeighbors class used for managing propagation dynamics for the computation of the perturbation in MASFENON.
+ * @details The PropagationModelNeighbors class provides methods for applying propagation logic to the perturbation computation.
+ * @details The propagation class uses a scale function to determine the scaling of the propagation term. The scale function can be set and modified as needed.
+ * @details To set the scale function, @see CustomFunctions.hxx
+ * @details The propagation function propagates the values in each network, considering each node's neighbors and the weights of the edges connecting them.
+ */
 #pragma once
 #include <armadillo>
 #include "computation/PropagationModel.hxx"
