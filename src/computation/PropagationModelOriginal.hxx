b335f8de4931a8af83f3a6585749244147110648
josura <giorgiolocicero@hotmail.com>
2025-05-13 13:27:13 +0000

    documentation: doxygen header for PropagationModelOriginal.hxx

--- src/computation/PropagationModelOriginal.hxx
+++ src/computation/PropagationModelOriginal.hxx
@@ -1,3 +1,12 @@
+/**
+ * @file PropagationModelOriginal.hxx
+ * @ingroup Core
+ * @brief Defines the PropagationModelOriginal class used for managing propagation dynamics for the computation of the perturbation in MASFENON.
+ * @details The PropagationModelOriginal class provides methods for applying propagation logic to the perturbation computation.
+ * @details The propagation class uses a scale function to determine the scaling of the propagation term. The scale function can be set and modified as needed.
+ * @details To set the scale function, @see CustomFunctions.hxx
+ * @details The propagation function propagates the values in each network, considering the whole network and the weights of the edges connecting them.
+ */
 #pragma once
 #include <armadillo>
 #include "computation/PropagationModel.hxx"
