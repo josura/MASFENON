f995d92e73112212ff8c24f17f190531afafb46a
josura <giorgiolocicero@hotmail.com>
2025-05-14 15:12:17 +0000

    documentation: doxygen header for the DissipationModelVectorized.cxx

--- src/computation/DissipationModelVectorized.cxx
+++ src/computation/DissipationModelVectorized.cxx
@@ -1,1 +1,6 @@
+/**
+ * @file DissipationModelVectorized.cxx
+ * @ingroup Experimental
+ * @brief Implements the DissipationModelVectorized class used for managing vectorized dissipation dynamics for the computation of the perturbation in MASFENON. This class is an abstract class.
+ */
 #include "computation/DissipationModelVectorized.hxx"
