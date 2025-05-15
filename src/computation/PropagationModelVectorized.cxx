022e71a8a80f4625e83a657b26d189131cb51047
josura <giorgiolocicero@hotmail.com>
2025-05-15 13:17:44 +0000

    documentation: doxygen header for the PropagationModelVectorized.cxx

--- src/computation/PropagationModelVectorized.cxx
+++ src/computation/PropagationModelVectorized.cxx
@@ -1,1 +1,6 @@
+/**
+ * @file PropagationModelVectorized.cxx
+ * @ingroup Experimental
+ * @brief Implements the PropagationModelVectorized class used for managing vectorized propagation dynamics for the computation of the perturbation in MASFENON. This class is an abstract class.
+ */
 #include "computation/PropagationModelVectorized.hxx"
