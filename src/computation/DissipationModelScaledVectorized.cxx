267323ec08d53b5c0e98508ef206984be928f1fe
josura <giorgiolocicero@hotmail.com>
2025-05-15 16:14:03 +0000

    documentation: doxygen header for the DissipationModelScaledVectorized.cxx

--- src/computation/DissipationModelScaledVectorized.cxx
+++ src/computation/DissipationModelScaledVectorized.cxx
@@ -1,1 +1,6 @@
+/**
+ * @file DissipationModelScaledVectorized.cxx
+ * @ingroup Experimental
+ * @brief Implements the DissipationModelScaledVectorized class used for managing vectorized scaled dissipation dynamics for the computation of the perturbation in MASFENON. This class is an abstract class.
+ */
 #include "computation/DissipationModelScaledVectorized.hxx"
