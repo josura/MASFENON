9f1bf4a32c7a1841fd28af598c53921f71cc9bbf
josura <giorgiolocicero@hotmail.com>
2025-05-21 13:05:01 +0000

    feature: comment header explaining what the new script does

---
+++ scripts/bash/singleCell/createNewParametersFromMSEtable.sh
@@ -0,0 +1,3 @@
+# returning the new parameters ranges after computing them from the MSE table passed with command line argument
+## The table has the following data(header is "MSE experiment")
+## <MSE-value> dissipation_<dissValue>-propagation_<propaValue>-conservation_<consValue>
