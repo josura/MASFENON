b8b246a3dbd36aa06a55a31b574a1466d0e8691f
josura <giorgiolocicero@hotmail.com>
2025-05-24 17:19:40 +0000

    feature: trying to make the bash script used for the fitting more generalized

--- scripts/bash/singleCell/fit-and-run-simulation.sh
+++ scripts/bash/singleCell/fit-and-run-simulation.sh
@@ -19,42 +19,149 @@
 ## dissipationMin dissipationMax dissipationSteps propagationMin propagationMax propagationSteps conservationMin conservationMax conservationStep
 # - GENERATE_NEW_PARAMS_SCRIPT for generating the new parameters from one MSE table generated with the previous script
 
-# Input parameters
-if [ "$#" -ne 5 ]; then
-    echo "Usage: $0 <epochs> <OutputFolder> <minScaleParameter> <maxScaleParameter> <intervalParameter>"
-    exit 1
-fi
+
+#! /bin/bash
+# This bash script will fit the framework with the data that is used for the single cell experiments. In this case, it will fit the simulation to better represent a single type of cell metabolite layer (AT1) in our scRNA-seq data.
+# From the MSE table, the fitting will 
+# The script takes the following command line parameters:
+## - The number of epochs:
+## - The MSE output folder where to save the MSE results:
+## - The initial scaling parameters <min,max,intervals> to start the simulation (they will be the same for each scaling parameter, meaning they will start from the same minimum and maximum, with the same interval) 
+# Every epoch generates three MSE files, representing the three timepoint that we have available for our data (6h,7h,10h)
+# TODO make the script more generic for different kind of data.
+# the bash/python script used to generate the MSE ranking for each timepoint are :
+# - SIMULATION_SCRIPT for generating the MSE sorted tables for each experiment:  /home/josura/Projects/ccc/c2c-sepia/scripts/python/temporalSingleCell/compute-MSE-metabolites.py
+## This python code generates the MSE sorted tables, from the single cell data, this code is available in the c2c-sepia repository at https://github.com/josura/c2c-sepia
+## The data used to fit the experiments is unavailable to the public at the moment
+## IMPORTANT: This python code can be substituted with a code that generates some MSE sorted tables in <mse_experiment.tsv> files with the following schema:
+### MSE	experiment
+### <MSE-value> dissipation_<dissValue>-propagation_<propaValue>-conservation_<consValue>
+# - COMPUTE_MSE_SCRIPT for running the simulation with custom parameters: /home/josura/Projects/ccc/MASFENON/scripts/bash/singleCell/simulation_custom_parameters.sh
+## This bash script run the masfenon simulation with the selected parameters on the data defined in the configuration section, the parameter used are the following:
+## dissipationMin dissipationMax dissipationSteps propagationMin propagationMax propagationSteps conservationMin conservationMax conservationStep
+# - GENERATE_NEW_PARAMS_SCRIPT for generating the new parameters from one MSE table generated with the previous script
+
+
+VENV_FOLDER="/home/josura/Projects/ccc/c2c-sepia/scripts/python/temporalSingleCell/venv"
+
+# Sourcing python environment for the python code generating the MSE tables
+source "$VENV_FOLDER/bin/activate"
+
 
 timepointToFit="6h"
 echo "[INFO] Timepoint to fit set to $timepointToFit"
 
-epochs=$1
-OutputFolder=$2
-minParam=$3
-maxParam=$4
-intervals=$5
+
+epochs=""
+OutputFolder=""
+
+# propagation, dissipation, conservation parameters
+minPropagationParam=""
+maxPropagationParam=""
+propagationIntervals=""
+minDissipationParam=""
+maxDissipationParam=""
+dissipationIntervals=""
+minConservationParam=""
+maxConservationParam=""
+conservationIntervals=""
+
+
+# ------------------------------
+# Input Argument Parsing Section
+# ------------------------------
+
+if [ "$#" -eq 5 ]; then
+    # Manual range mode
+    mode="manual"
+    echo "[INFO] Using manual parameter ranges: [$minParam, $maxParam] with $intervals intervals"
+    epochs=$1
+    OutputFolder=$2
+    # Reading the command line parameters
+    minParam=$3
+    maxParam=$4
+    intervals=$5
+    # propagation, dissipation, conservation parameters, from the command line parameters
+    minPropagationParam=$minParam
+    maxPropagationParam=$maxParam
+    propagationIntervals=$intervals
+    minDissipationParam=$minParam
+    maxDissipationParam=$maxParam
+    dissipationIntervals=$intervals
+    minConservationParam=$minParam
+    maxConservationParam=$maxParam
+    conservationIntervals=$intervals
+    
+elif [ "$#" -eq 2 ]; then
+    # Resume mode from file
+    mode="resume computation"
+    echo "[INFO] Computing ranges from simulation output folder"
+    epochs=$1
+    OutputFolder=$2
+    # Create a subfolder for this epoch's outputs
+    epochOutput="$OutputFolder/epoch_initial"
+    mkdir -p "$epochOutput"
+    # Infer the new parameters from the simulated data
+    echo "[INFO] Computing MSE for initial parameter selection from simulation output folder"
+    # TODO also make the intervals be passed as command line argument
+    python3 $COMPUTE_MSE_SCRIPT $epochOutput
+
+    echo "[DONE] Epoch $epoch completed."
+    echo
+    # update the parameters by running the generation of the new parameters script
+    bash "$GENERATE_NEW_PARAMS_SCRIPT" \
+        "$epochOutput/mse_$timepointToFit.tsv" \
+        5 \
+        "$intervals" \
+        "$epochOutput/newParams_mse_$timepointToFit.txt"
+    # Read updated parameters
+    while IFS= read -r line; do
+        key=$(echo "$line" | cut -d':' -f1)
+        min=$(echo "$line" | grep -o 'min=[^,]*' | cut -d'=' -f2)
+        max=$(echo "$line" | grep -o 'max=[^,]*' | cut -d'=' -f2)
+        steps=$(echo "$line" | grep -o 'intervals=[^ ]*' | cut -d'=' -f2)
+
+        case "$key" in
+            dissipation)
+                minDissipationParam="$min"
+                maxDissipationParam="$max"
+                dissipationIntervals="$steps"
+                ;;
+            propagation)
+                minPropagationParam="$min"
+                maxPropagationParam="$max"
+                propagationIntervals="$steps"
+                ;;
+            conservation)
+                minConservationParam="$min"
+                maxConservationParam="$max"
+                conservationIntervals="$steps"
+                ;;
+        esac
+    done < "$epochOutput/newParams_mse_${timepointToFit}.txt"
+    
+else
+    echo "Usage:"
+    echo "  $0 <epochs> <OutputFolder> <minParam> <maxParam> <intervals>      # manual range mode"
+    echo "  $0 <epochs> <OutputFolder>                                        # resume from previous simulations"
+    exit 1
+fi
+
+
+# Input parameters
+## TODO if 2 parameters are specified, compute the new parameters from the simulation output folder
+## TODO else if 5 parameters are specified, start from the specified minimum scale parameter and max scale parameter
+## else print usage and exit
+if [ "$#" -ne 5 ]; then
+    echo "Usage: $0 <epochs> <OutputFolder> <minScaleParameter> <maxScaleParameter> <intervalParameter>"
+    exit 1
+fi
 
 # Paths to scripts
 SIMULATION_SCRIPT="/home/josura/Projects/ccc/MASFENON/scripts/bash/singleCell/simulation_custom_parameters.sh"
 COMPUTE_MSE_SCRIPT="/home/josura/Projects/ccc/c2c-sepia/scripts/python/temporalSingleCell/compute-MSE-metabolites.py"
 GENERATE_NEW_PARAMS_SCRIPT="/home/josura/Projects/ccc/MASFENON/scripts/bash/singleCell/createNewParametersFromMSEtable.sh"
 
-VENV_FOLDER="/home/josura/Projects/ccc/c2c-sepia/scripts/python/temporalSingleCell/venv"
-
-
-# propagation, dissipation, conservation parameters
-minPropagationParam=$minParam
-maxPropagationParam=$maxParam
-propagationIntervals=$intervals
-minDissipationParam=$minParam
-maxDissipationParam=$maxParam
-dissipationIntervals=$intervals
-minConservationParam=$minParam
-maxConservationParam=$maxParam
-conservationIntervals=$intervals
-
-# Sourcing python environment for the python code generating the MSE tables
-source "$VENV_FOLDER/bin/activate"
 # Epoch loop
 for ((epoch=1; epoch<=epochs; epoch++)); do
     echo "=== Epoch $epoch / $epochs ==="
@@ -68,13 +175,15 @@ for ((epoch=1; epoch<=epochs; epoch++)); do
     mkdir -p "$epochOutput"
 
     echo "[INFO] Running simulation (epoch $epoch)..."
-    bash "$SIMULATION_SCRIPT" \
-        "$minDissipationParam" "$maxDissipationParam" "$dissipationIntervals" \
-        "$minPropagationParam" "$maxPropagationParam" "$propagationIntervals" \
-        "$minConservationParam" "$maxConservationParam" "$conservationIntervals"
+    # TODO make the simulation output folder be passed via command argument in case
+    # bash "$SIMULATION_SCRIPT" \
+    #     "$minDissipationParam" "$maxDissipationParam" "$dissipationIntervals" \
+    #     "$minPropagationParam" "$maxPropagationParam" "$propagationIntervals" \
+    #     "$minConservationParam" "$maxConservationParam" "$conservationIntervals"
 
     echo "[INFO] Computing MSE for epoch $epoch..."
-    python3 "$COMPUTE_MSE_SCRIPT" --outputFolder "$epochOutput"
+    # TODO also make the intervals be passed as command line argument
+    python3 $COMPUTE_MSE_SCRIPT $epochOutput
 
     echo "[DONE] Epoch $epoch completed."
     echo
