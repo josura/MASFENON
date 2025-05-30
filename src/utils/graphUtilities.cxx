652dabc439bca5f97e13b5704061f7620eb8940c
josura <giorgiolocicero@hotmail.com>
2025-05-30 15:11:33 +0000

    feature: started working on the namespace for the graph utilities related to the WeightedEdgeGraph class

--- src/utils/graphUtilities.cxx
+++ src/utils/graphUtilities.cxx
@@ -1,1 +1,14 @@
 #include "utils/graphUtilities.hxx"
+
+double weighed_graph_metrics::averageEdgeWeight(const WeightedEdgeGraph& graph){
+    if (graph.getNumEdges() == 0) {
+        return 0.0; // Avoid division by zero if there are no edges
+    }
+    
+    double totalWeight = 0.0;
+    for (const auto& edge : graph.edgesVector) {
+        totalWeight += std::get<2>(edge); // Get the weight from the tuple (node1ID, node2ID, weight)
+    }
+    
+    return totalWeight / graph.getNumEdges(); // Return the average weight
+}
--- src/utils/graphUtilities.hxx
+++ src/utils/graphUtilities.hxx
@@ -7,3 +7,19 @@
 #pragma once
 #include <vector>
 #include "data_structures/WeightedEdgeGraph.hxx"
+
+/**
+ * @namespace weighed_graph_metrics
+ * @brief Contains utility functions for calculating metrics on weighted graphs.
+ * @details The functions are used to calculate metrics such as the average weight of edges in a graph.
+ * @note The functions are designed to work with the WeightedEdgeGraph class.
+ */
+namespace weighed_graph_metrics {
+    /**
+     * @brief Calculates the average weight of edges in a weighted graph.
+     * @param graph The weighted edge graph to analyze.
+     * @return The average weight of the edges in the graph.
+     * @details This function iterates through all edges in the graph and computes the average weight.
+     */
+    double averageEdgeWeight(const WeightedEdgeGraph& graph);
+}
