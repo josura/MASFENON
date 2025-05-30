3ad22f563d842ab49b19cb79475669f962ee29f8
josura <giorgiolocicero@hotmail.com>
2025-05-30 15:05:37 +0000

    feature: init graphUtilities files and started including necessary libraris

---
+++ src/utils/graphUtilities.cxx
@@ -0,0 +1,1 @@
+#include "utils/graphUtilities.hxx"
---
+++ src/utils/graphUtilities.hxx
@@ -0,0 +1,9 @@
+/**
+ * @file graphUtilities.hxx
+ * @ingroup Core
+ * @brief Contains utility functions for graph operations.
+ * @details The functions are used to work with graphs, and finding metrics
+ */
+#pragma once
+#include <vector>
+#include "data_structures/WeightedEdgeGraph.hxx"
