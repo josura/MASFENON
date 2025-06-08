7a181fe855391f0733099edf3037a99fce92be31
josura <giorgiolocicero@hotmail.com>
2025-06-08 22:37:19 +0200

    feature: continuing with the implementation of a more complex conservation scaling function with parameters for the use in the single cell experiments

--- src/CustomFunctions.cxx
+++ src/CustomFunctions.cxx
@@ -78,7 +78,16 @@ std::function<double(double)> getConservationScalingFunction(std::vector<double>
     return [parameters](double x) {
         // Constant conservation factor returned as the first parameter (can be modified dynamically)
         // Parameters can be used to adjust the factor if needed
-        return parameters[0]; // Example constant value from parameters
+        if(x <= 5.0){
+            return parameters[0]; // Example constant value from parameters
+        } else if (x > 5.0 && x <= 6.0) {
+            return parameters[1]; // Example different constant value from parameters
+        }
+        else if (x > 6.0 && x <= 10.0) {
+            return parameters[2]; // Example another constant value from parameters
+        } else {
+            return parameters[2]; // Default value if none of the conditions are met
+        }
     };
 }
 
