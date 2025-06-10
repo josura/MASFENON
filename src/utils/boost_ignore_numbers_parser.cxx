229b74970ba50fcf412ca97d6cb9b58fc3a3881e
josura <giorgiolocicero@hotmail.com>
2025-06-10 12:59:14 +0200

    feature: added ignore numbers parser implementation

---
+++ src/utils/boost_ignore_numbers_parser.cxx
@@ -0,0 +1,11 @@
+#include <vector>
+#include <string>
+#include <boost/program_options/option.hpp>
+#include <boost/lexical_cast/try_lexical_convert.hpp>
+#include <boost/program_options/value_semantic.hpp>
+
+
+
+using po = boost::program_options;
+
+std::vector<po::option> ignore_numbers(std::vector<std::string>& args);
