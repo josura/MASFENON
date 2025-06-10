db52ebebb4448a70bb434da49739559533ece311
josura <giorgiolocicero@hotmail.com>
2025-06-10 14:27:04 +0200

    documentation: added some documentation for the function to ignore parsing

--- src/utils/boost_ignore_numbers_parser.cxx
+++ src/utils/boost_ignore_numbers_parser.cxx
@@ -3,17 +3,17 @@
 std::vector<po::option> ignore_numbers(std::vector<std::string>& args)
 {
     std::vector<po::option> result;
-    int pos = 0;
+    int position = 0;
     while(!args.empty()) {
         const auto& arg = args[0];
         double num;
         if(boost::conversion::try_lexical_convert(arg, num)) {
             result.push_back(po::option());
-            po::option& opt = result.back();
+            po::option& option = result.back();
 
-            opt.position_key = pos++;
-            opt.value.push_back(arg);
-            opt.original_tokens.push_back(arg);
+            option.position_key = position++;
+            option.value.push_back(arg);
+            option.original_tokens.push_back(arg);
 
             args.erase(args.begin());
         } else {
--- src/utils/boost_ignore_numbers_parser.hxx
+++ src/utils/boost_ignore_numbers_parser.hxx
@@ -1,3 +1,9 @@
+/**
+ * @file boost_ignore_numbers_parser.hxx
+ * @ingroup Core
+ * @brief  This file contains the declaration of the function used to ignore numbers (negative numbers) when parsing command line options 
+ * @details  The function used to ignore numbers works with the arguments and returns a vector of program options
+ */
 #include <vector>
 #include <string>
 #include <boost/program_options.hpp>
@@ -9,4 +15,10 @@
 
 namespace po = boost::program_options;
 
+/**
+ * @brief Function used to ignore numbers whern parsing
+ * @param args the command line parameters
+ * @return The vector of program options
+ * @details This checks if the token is a number (including negatives) and treats it as a positional option rather than a flag.
+ */
 std::vector<po::option> ignore_numbers(std::vector<std::string>& args);
