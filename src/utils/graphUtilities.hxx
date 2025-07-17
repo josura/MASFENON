e12bd76b6b854d24d8f4cd6b947dd1a94e4e1f16
josura <giorgiolocicero@hotmail.com>
2025-07-17 17:15:41 +0200

    feature: dijkstra's algorithm declaration

--- src/utils/graphUtilities.hxx
+++ src/utils/graphUtilities.hxx
@@ -176,5 +176,22 @@ namespace weighted_graph_metrics {
     > allUnweightedShortestPathBFS(const WeightedEdgeGraph& graph, int source);
 
 
+    /**
+     * @brief Computes the weighted shortest path with Dijkstra's algorithm from a source node to all other nodes in a weighted edge graph.
+     * @param graph The weighted edge graph to analyze.
+     * @param source The source node from which to compute the shortest paths.
+     * @return A vector pairs containing the shortest path lengths and the corresponding paths from the source node to all other nodes.
+     * @note The paths are represented as vectors of node indices.
+     * @throw std::out_of_range if the source node index is out of range.
+     * @throw std::invalid_argument if the graph is empty.
+     * @details This function uses Dijkstra's algorithm to compute the shortest path lengths in a weighted graph.
+     * @note The graph is treated as weighted, meaning that edges can have different weights.
+     */
+    std::vector< 
+        std::pair< 
+            int,std::vector<int>
+        >
+    > allWeightedShortestPathDijkstra(const WeightedEdgeGraph& graph, int source);
+
 
 }
