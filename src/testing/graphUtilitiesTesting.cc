cf84ee877afb93143a3a43583b49959cf1394a3e
josura <giorgiolocicero@hotmail.com>
2025-07-26 17:06:00 +0200

    testing: added new graph with no cycles to the testing suite

--- src/testing/graphUtilitiesTesting.cc
+++ src/testing/graphUtilitiesTesting.cc
@@ -11,6 +11,7 @@ class GraphUtilitiesTesting : public ::testing::Test {
             graph2 = new WeightedEdgeGraph(5); // Graph with 5 nodes, no edges
             graph3 = new WeightedEdgeGraph(nodeNames); // Graph with 5 nodes
             graph4 = new WeightedEdgeGraph(nodeNames, nodeValues); // Graph with 5 nodes and values
+            graph5 = new WeightedEdgeGraph(nodeNames, nodeValues); // Graph with 5 nodes, no cycles
 
             // Add edges to graph3
             graph3->addEdge("node1", "node2", 1.0);
@@ -34,6 +35,12 @@ class GraphUtilitiesTesting : public ::testing::Test {
             graph4->addEdge("node3", "node5", 3.5);
             graph4->addEdge("node4", "node1", 4.5);
             graph4->addEdge("node5", "node2", 5.5);
+            // add edges to graph5
+            graph5->addEdge("node1", "node2", 1.0);
+            graph5->addEdge("node2", "node3", 2.0);
+            graph5->addEdge("node1", "node3", 3.0);
+            graph5->addEdge("node4", "node1", 4.0);
+            graph5->addEdge("node5", "node4", 5.0);
         }
 
         void TearDown() override {
@@ -42,6 +49,7 @@ class GraphUtilitiesTesting : public ::testing::Test {
             delete graph2;
             delete graph3;
             delete graph4;
+            delete graph5;
         }
 
         // member variables for the test class
@@ -49,6 +57,7 @@ class GraphUtilitiesTesting : public ::testing::Test {
         WeightedEdgeGraph *graph2; // graph with 5 nodes and no edges
         WeightedEdgeGraph *graph3; // graph with 5 nodes, no values and 10 edges
         WeightedEdgeGraph *graph4; // graph with 5 nodes and values and 10 edges
+        WeightedEdgeGraph *graph5; // graph with 5 nodes and 5 edges, no cycles
 
         std::vector<std::string> nodeNames{"node1","node2","node3","node4","node5"};
         std::vector<double> nodeValues{0.3,4.1,3.8,8.2,9.5};
