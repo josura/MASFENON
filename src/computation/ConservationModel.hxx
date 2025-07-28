377a59eeae7be4264f98c9fea7f3d7bf0948075c
josura <giorgiolocicero@hotmail.com>
2025-07-28 21:35:45 +0200

    feature: init starting to implement vectorized scaling function in Conservation Model

--- src/computation/ConservationModel.hxx
+++ src/computation/ConservationModel.hxx
@@ -34,6 +34,12 @@ class ConservationModel{
          */
         ConservationModel(std::function<double(double)> scaleFunction);
         /**
+         * @brief Constructor for the Conservation Model class, passing a scaling function that uses vectorized operations.
+         * @param scaleFunction The function to scale the conservation term. It takes a double value (time) and returns a vector of double values (scaling values).
+         * @details Initializes the scale function to the provided value, allowing for vectorized operations on the input vector.
+         */
+        ConservationModel(std::function<arma::Col<double>(double)> scaleFunction);
+        /**
          * @brief Destructor for the ConservationModel class.
          * @details Cleans up any resources used by the class.
          */
