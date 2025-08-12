34f8b1029d6e4a25edacf6667324ba7d3c2a12b3
josura <giorgiolocicero@hotmail.com>
2025-08-13 00:42:48 +0200

    testing: init function to test if the function to read the dissipation parameters is working, to implement

--- src/testing/utilitiesTesting.cc
+++ src/testing/utilitiesTesting.cc
@@ -1,8 +1,20 @@
+/**
+ * @file utilitiesTesting.cc
+ * @ingroup Testing
+ * @brief Contains unit tests for the utilities functions in MASFENON.
+ * @details The tests cover various functionalities such as file reading, dissipation scaling functions, and more.
+ * @warning This file is intended for testing purposes only and should not be used in production code.
+ * @todo Add more tests for other utility functions.
+ * @todo Add more tests for getting dissipation scaling functions with different parameters. Especially controlling the behaviour with different number of parameters(throwing exception inside)
+ * @see utilities.hxx
+ * @see utilities.cxx
+ */
+
 #include <gtest/gtest.h>
 #include <iostream>
 #include <string>
 #include <vector>
-#include "utils/mathUtilities.hxx"
+#include "utils/utilities.hxx"
 
 class utilitiesTesting : public ::testing::Test {
     protected:
@@ -15,6 +27,7 @@ class utilitiesTesting : public ::testing::Test {
             //delete resources
         }
         // protected variables here
+        std::vector<std::string> orderedNodeNames_t0 = {"a0","b0","c0","e0","d0","f0"}; 
 
   
 };
@@ -23,3 +36,9 @@ TEST_F(utilitiesTesting, testWorks) {
     EXPECT_EQ(1, 1);
     
 }
+
+TEST_F(utilitiesTesting, dissipationScalingFunctionFromFileWorksFullParametersOrdered) {
+    // std::string fileName = "data/testdata/testHeterogeneousTemporalGraphMultipleInteractions/parameters/dissipationParameters/t0.tsv";
+    // auto scaleFunction = dissipationScalingFunctionFromFile(fileName, orderedNodeNames_t0);
+}
+    
