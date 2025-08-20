fbde30fc8d3d2a700c3befd4b7e9926a27740f70
josura <giorgiolocicero@hotmail.com>
2025-08-20 18:16:54 +0200

    feature: added comments to explain what the error matrix creation script does and what it needs

--- scripts/generalFitting/createErrorMatrix.py
+++ scripts/generalFitting/createErrorMatrix.py
@@ -5,3 +5,6 @@ This script creates an error matrix for fitting models to synthetic time series 
 This script takes one simulation output folder as input, and a real data time series folder as input. The files inside each folder are expected to be from the MASFENON output, that is
 - The folder for the simulation output should contain files named `t0.tsv`, `t1.tsv`, etc., which represent the different agent names, inside these files, the header is composed by the first column "nodeNames", the other columns are the timepoints. The  rows are the node name and the value at that timepoint.
 - The folder for the real data time series should contain files named `t0.tsv`, `t1.tsv`, etc., which represent the different agent names, inside these files, the header is composed by the first column "nodeNames", the other columns are the timepoints. The rows are the node name and the value at that timepoint.
+
+The script will create an error matrix in a file specified by the user. The error matrix will contain the differences between the simulated and real data for each time point and node.
+"""
