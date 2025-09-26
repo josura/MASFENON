82df527a06e2c17d267ca2c21ec447162aa92b7e
josura <giorgiolocicero@hotmail.com>
2025-09-26 09:24:32 +0200

    feature: init python script for plotting RMSE

---
+++ scripts/generalFitting/plotRMSE_file.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""
+Plot RMSE from a file containing RMSE values.
+Input file format:
+- TSV with two columns, first column is the epoch, second column is the RMSE value.
+"""
