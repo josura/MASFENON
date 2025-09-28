43b6b9c343929f4bcb69ea196a182c104afc648e
josura <giorgiolocicero@hotmail.com>
2025-09-28 17:34:11 +0200

    feature: starting the implementation of the finer fitting driver

---
+++ scripts/generalFitting/fit_driver_finer_select.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# -----------------------------
+# MAS Fitting Driver (Bash), with finer gradient building(single parameter perturbation), and selection of network to fit
+# -----------------------------
