370ef1f15d1766676496becbcc2468f89cd1df52
josura <giorgiolocicero@hotmail.com>
2025-10-04 19:57:18 +0200

    documentation: added a todo to remember to add a future control over the error of other parameters from the selected ones

--- scripts/generalFitting/fit_driver_finer_select.sh
+++ scripts/generalFitting/fit_driver_finer_select.sh
@@ -11,3 +11,5 @@ set -euo pipefail
 #     createParametersIterationAware-singleParameter.py
 #     computeRMSE-fromErrorMatrix.py
 # - MASFENON simulator executable (invoked under mpirun)
+# -----------------------------
+# TODO - add threshold when the errors of other parameters are too high, meaning that the single parameter perturbation has bigger consequences for other parameters than the one being perturbed
