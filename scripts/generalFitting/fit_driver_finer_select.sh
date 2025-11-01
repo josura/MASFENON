77af1709a381bb2a5579f7ea2e697d693f849f20
josura <giorgiolocicero@hotmail.com>
2025-11-01 19:49:46 +0100

    feature: testing implementation of perturb single parameter in the file

--- scripts/generalFitting/fit_driver_finer_select.sh
+++ scripts/generalFitting/fit_driver_finer_select.sh
@@ -171,5 +171,10 @@ write_uniform_params_for_type() {
 }
 
 perturb_single_parameter_in_a_file(){
+  # $1=input_file $2=output_file $3=node_name $4=timepoint_index $5=step_size
+  local input_file="$1" output_file="$2" node_name="$3" timepoint_index="$4" step_size="$5"
+  awk -v nn="$node_name" -v ti="$timepoint_index" -v ss="$step_size" 'BEGIN{FS=OFS="\t"} 
+    NR==1{print; next} 
+    {if($1==nn){$ti+ss} print}' "$input_file" > "$output_file"
   
 }
